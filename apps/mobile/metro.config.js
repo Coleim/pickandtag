df4ac818669872f3f0b37dff1766109983a2d171
Clément Oliva <clement.oliva@gmail.com>
2025-12-14 18:02:03 +0100

    Move existing Expo app to apps/mobile

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,5 +1,6 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
@@ -14,6 +15,19 @@ config.server.enhanceMiddleware = (middleware) => {
     res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
     middleware(req, res, next);
   };
+};
+
+// --------------------
+// Monorepo: include packages/* so Metro can resolve them
+config.watchFolders = [
+  path.resolve(__dirname, '../../packages') // adapte selon la structure: root/apps/mobile
+];
+
+// --------------------
+// Optional: if you get duplicate React errors
+config.resolver.extraNodeModules = {
+  react: path.resolve(__dirname, 'node_modules/react'),
+  'react-native': path.resolve(__dirname, 'node_modules/react-native'),
 };
 
 
